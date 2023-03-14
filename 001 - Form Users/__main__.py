d10871aeffad7bac492a42ee002f62bff71c0860
informaticaeloy <eloyvilla@gmail.com>
2023-03-14 22:21:01 +0100

    Add files via upload

---
+++ 001 - Form Users/__main__.py
@@ -0,0 +1,16 @@
+from uvicorn import Config, Server
+
+
+server = Server(
+    Config(
+        "main:app",
+        host="127.0.0.1",
+        port=9002,
+        reload=True,
+        log_level='debug' # Options: 'critical', 'error', 'warning', 'info', 'debug', 'trace'
+    ),
+ )
+    
+
+
+server.run()
--- 001 - Form Users/main.py
+++ 001 - Form Users/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import requests
 
+
+templates = Jinja2Templates(directory='templates')
 
 app = FastAPI()
 
@@ -9,14 +13,29 @@ app.mount("/statics_img", StaticFiles(directory="./templates/statics/images"), n
 app.mount("/statics_css", StaticFiles(directory="./templates/statics/css"), name='css')
 
 
-@app.post("/login", response_class=HTMLResponse)
-async def login(username: str = Form(...), password: str = Form(...), ipaddress: str = Form(...)):
-    print(username)
-    print(password)
-    print(ipaddress)
-    return {"username": username, "password": password, "ipaddress": ipaddress}
+#@app.post("/login", response_class=HTMLResponse)
+#async def login(username: str = Form(...), password: str = Form(...), ipaddress: str = Form(...)):
+#    print(username)
+#    print(password)
+#    print(ipaddress)
+#    return {"username": username, "password": password, "ipaddress": ipaddress}
 
+@app.post('/login')
+async def login(username: str = Form(...), password: str = Form(...), public_ip: str = Form(...)):
+
+    return {"username" : username, "password" : password, "public_ip" : public_ip}
+
+#@app.get('/', response_class=HTMLResponse)
+#def index():
+#    with open("./templates/index.html", 'r') as f:
+#        return HTMLResponse(content=f.read())
+    
 @app.get('/', response_class=HTMLResponse)
-def index():
-    with open("./templates/index.html", 'r') as f:
-        return HTMLResponse(content=f.read())
+def main(request: Request):
+    public_ip = requests.get('https://checkip.amazonaws.com').text.rstrip()
+    #r.json()
+    #return templates.TemplateResponse('index.html', context = {'request': request, 'public_ip': public_ip})
+    return templates.TemplateResponse('index.html', context = {'request': request, 'public_ip': public_ip})
+
+
+
