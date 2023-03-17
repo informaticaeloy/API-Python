c5f8499fa6b8b15d58313145375c5640d3f7bcb4
informaticaeloy <eloyvilla@gmail.com>
2023-03-17 13:13:05 +0100

    Update main.py

--- 001 - Formulario Login POST/main.py
+++ 001 - Formulario Login POST/main.py
@@ -11,16 +11,11 @@ app = FastAPI()
 app.mount("/statics_img", StaticFiles(directory="./templates/statics/images"), name='images')
 app.mount("/statics_css", StaticFiles(directory="./templates/statics/css"), name='css')
 
-@app.post('/login')
-async def login(username: str = Form(...), password: str = Form(...), public_ip: str = Form(...)):
-    datos_login = {"username" : username, "password" : password, "public_ip" : public_ip}
-    return datos_login
-    
-
 @app.get('/', response_class=HTMLResponse)
 def main(request: Request):
     public_ip = requests.get('https://checkip.amazonaws.com').text.rstrip()
     return templates.TemplateResponse('index.html', context = {'request': request, 'public_ip': public_ip})
-
-
-
+@app.post('/login')
+async def login(username: str = Form(...), password: str = Form(...), public_ip: str = Form(...)):
+    datos_login = {"username" : username, "password" : password, "public_ip" : public_ip}
+    return datos_login
